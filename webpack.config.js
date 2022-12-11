2eff1408aec6b23c1af5ef9b0b2d897767149a03
rubenTNC <rubenen@mail.ru>
2022-12-11 14:28:11 +0300

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCSSExtractPlugin.loader,
+                    'css-loader',
+                ]
+            }
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: './main.html',
+        }),
+        new MiniCSSExtractPlugin({
+            filename: 'main.css',
+        })
+    ]
+};
